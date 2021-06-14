87ff8467c76cceba2018b167bb5dc1df654e4735
Matt Inamdar <matt@goldspecdigital.com>
2021-06-14 11:08:01 +0100

    First commit

---
+++ patterns-of-enterprise-application-architecture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace patterns_of_enterprise_application_architecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
